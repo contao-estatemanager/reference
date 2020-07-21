19f7b6825b22796b73436740513717433285df29
eki89 <fabian@oveleon.de>
2020-07-21 16:04:32 +0200

    Bugfix: Wrong HOOK method renamed to prevent error during real estate imports.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -18,7 +18,7 @@ if(ContaoEstateManager\Reference\AddonManager::valid()) {
     $GLOBALS['TL_HOOKS']['getTypeParameterByGroups'][]     = array('ContaoEstateManager\Reference\Reference', 'setFilterParameter');
     $GLOBALS['TL_HOOKS']['getSimilarFilterOptions'][]      = array('ContaoEstateManager\Reference\Reference', 'setSimilarFilterParameter');
     $GLOBALS['TL_HOOKS']['compileRealEstateExpose'][]      = array('ContaoEstateManager\Reference\Reference', 'compileRealEstateExpose');
-    $GLOBALS['TL_HOOKS']['realEstateImportDeleteRecord'][] = array('ContaoEstateManager\Reference\Reference', 'isDeleteAllowed');
+    $GLOBALS['TL_HOOKS']['realEstateImportDeleteRecord'][] = array('ContaoEstateManager\Reference\Reference', 'realEstateImportDeleteRecord');
 
     $GLOBALS['TL_HOOKS']['getStatusTokens'][]              = array('ContaoEstateManager\Reference\Reference', 'addStatusToken');
     $GLOBALS['TL_HOOKS']['getMainDetails'][]               = array('ContaoEstateManager\Reference\Reference', 'removeReferenceMainDetails');
